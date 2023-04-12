3b9c2318b2746d1d172bb609acef1a6e29d1a3b2
vyunistia <vonny.yunistia@gmail.com>
2023-04-12 16:06:57 -0400

    my prcatice of git

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am a project");
+
+// need to make repository in project folder which is gitClass3
+
+
+
+
+
+    }
+}
